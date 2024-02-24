4de25bb5e7676f1c6e503430db4e00af9706602a
FLiani <felipeliani@outlook.com>
2024-02-24 17:31:47 -0300

    cors

--- express/backend/src/router.js
+++ express/backend/src/router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const controller = require('./controllers/controller');
+const middleware = require('./middlewares/middleware');
+
 const router = express.Router()
 
-router.get("/", controller.getHelloWorld);
+router.get("/helloworld", controller.getHelloWorld);
 
 module.exports = router
--- express/backend/src/server.js
+++ express/backend/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router = require('./router')
+const cors = require('cors');
+const router = require('./router');
 const app = express();
 
-
-app.use(router)
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
 app.listen(3000, () => {
-  console.log("server rodando na porta 3000")
-})
+  console.log("Servidor rodando na porta 3000");
+});
